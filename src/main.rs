67b1189678e3af9ddf71ac9a2b1d6e558b250ac9
madclaws <pavanan.anandu63@gmail.com>
2022-01-30 21:27:14 +0530

    feat: rendered triangle

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use self::gl::types::*;
 use std::os::raw::c_void;
 use std::ffi::CString;
 use std::ptr;
+use std::str;
 
 use std::sync::mpsc::Receiver;
 
@@ -17,6 +18,14 @@ const VERTEX_SHADER_SOURCE: &str = r#"
     layout (location = 0) in vec3 aPos;
     void main() {
         gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    }
+"#;
+
+const FRAGMENT_SHADER_SOURCE: &str = r#"
+    #version 330 core
+    out vec4 FragColor;
+    void main() {
+        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
     }
 "#;
 
@@ -48,27 +57,77 @@ fn main() {
         0.0, 0.5, 0.0
     ];
 
-    unsafe {
+    let (shader_program, VAO) = unsafe {
+        // vertex shader
         let vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
         let c_vertex_source = CString::new(VERTEX_SHADER_SOURCE.as_bytes()).unwrap();
         gl::ShaderSource(vertex_shader, 1, &c_vertex_source.as_ptr(), ptr::null());
         gl::CompileShader(vertex_shader);
 
-        // TODO: Validating the shader compilation
+        let mut success = gl::FALSE as GLint;
+        let mut info_log: Vec<u8> = Vec::with_capacity(512);
+        info_log.set_len(512 - 1);
+        gl::GetShaderiv(vertex_shader, gl::COMPILE_STATUS, &mut success);
+        if success != gl::TRUE as GLint {
+            gl::GetShaderInfoLog(vertex_shader, 512, ptr::null_mut(), info_log.as_mut_ptr() as *mut GLchar);
+            println!("ERROR::SHADER::VERTEX::COMPILATION_FAILED {:?}", str::from_utf8(&info_log).unwrap());
+        }
+
+        //  fragment shader
+        let fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
+        let c_fragment_source = CString::new(FRAGMENT_SHADER_SOURCE.as_bytes()).unwrap();
+        gl::ShaderSource(fragment_shader, 1, &c_fragment_source.as_ptr(), ptr::null());
+        gl::CompileShader(fragment_shader);
+        let mut success_fragment = gl::FALSE as GLint;
+        let mut info_log_fragment: Vec<u8> = Vec::with_capacity(512);
+        info_log_fragment.set_len(512 - 1);
+        gl::GetShaderiv(fragment_shader, gl::COMPILE_STATUS, &mut success_fragment);
+        if success_fragment != gl::TRUE as GLint {
+            gl::GetShaderInfoLog(fragment_shader, 512, ptr::null_mut(), info_log_fragment.as_mut_ptr() as *mut GLchar);
+            println!("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED {:?}", str::from_utf8(&info_log_fragment).unwrap());
+        }
+
+        // Shader program
+        let shader_program = gl::CreateProgram();
+        gl::AttachShader(shader_program, vertex_shader);
+        gl::AttachShader(shader_program, fragment_shader);
+        gl::LinkProgram(shader_program);
+        gl::GetProgramiv(shader_program, gl::LINK_STATUS, &mut success_fragment);
+        if success_fragment != gl::TRUE as GLint {
+            gl::GetProgramInfoLog(shader_program, 512, ptr::null_mut(), info_log_fragment.as_mut_ptr() as *mut GLchar);
+            println!("ERROR::SHADER::PROGRAM::COMPILATION_FAILED {:?}", str::from_utf8(&info_log_fragment).unwrap());
+        }
+
+        gl::DeleteShader(vertex_shader);
+        gl::DeleteShader(fragment_shader);
+
         let mut VBO = 0;
+        let mut VAO = 0;
         gl::GenBuffers(1, &mut VBO);
+        gl::GenVertexArrays(1, &mut VAO);
+        
+        gl::BindVertexArray(VAO);
         gl::BindBuffer(gl::ARRAY_BUFFER, VBO);
         gl::BufferData(gl::ARRAY_BUFFER, 
             vertices.len() as isize * mem::size_of::<GLfloat>() as GLsizeiptr,
             &vertices[0] as *const f32 as *const c_void,
             gl::STATIC_DRAW 
-        )
-    }
+        );
+        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 3 * mem::size_of::<GLfloat>() as GLsizei, ptr::null());
+        gl::EnableVertexAttribArray(0);
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl::BindVertexArray(0);
+        (shader_program, VAO)
+    };
     // render loop
     while !window.should_close() {
         unsafe {
             gl::ClearColor(0.2, 0.3, 0.3, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            gl::UseProgram(shader_program);
+            gl::BindVertexArray(VAO);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
         }
 
         process_input(&mut window, &events);
